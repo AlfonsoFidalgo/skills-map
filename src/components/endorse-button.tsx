310ed0fad49e190fad710fb57429e6412e6fa5a7
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2025-07-31 11:32:22 +0200

    Add modal

--- src/components/endorse-button.tsx
+++ src/components/endorse-button.tsx
@@ -1,10 +1,16 @@
+"use client";
+
+import React from "react";
+import { useSession } from "next-auth/react";
 import { signIn } from "@/actions/auth";
-import { auth } from "@/auth";
 
-export async function EndorseButton({ pageUserId }: { pageUserId: string }) {
-  const session = await auth();
+import EndorseModal from "./modal";
 
-  if (!session?.user) {
+export function EndorseButton({ pageUserId }: { pageUserId: string }) {
+  const session = useSession();
+  const [open, setOpen] = React.useState(false);
+
+  if (!session.data?.user) {
     return (
       <>
         <div className="border-t border-gray-200 mb-8"></div>
@@ -22,15 +28,19 @@ export async function EndorseButton({ pageUserId }: { pageUserId: string }) {
       </>
     );
   }
-  if (session.user.id !== pageUserId) {
+  if (session.data.user.id === pageUserId) {
     return (
       <>
         <div className="border-t border-gray-200 mb-8"></div>
         <div className="flex justify-center">
-          <button className="w-3/4 bg-gradient-to-r from-green-500 to-emerald-600 text-white px-6 py-4 rounded-xl font-semibold shadow-lg hover:from-green-600 hover:to-emerald-700 hover:shadow-xl transition-all duration-200 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring-green-300 text-lg">
+          <button
+            onClick={() => setOpen(true)}
+            className="w-3/4 bg-gradient-to-r from-green-500 to-emerald-600 text-white px-6 py-4 rounded-xl font-semibold shadow-lg hover:from-green-600 hover:to-emerald-700 hover:shadow-xl transition-all duration-200 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring-green-300 text-lg"
+          >
             Endorse
           </button>
         </div>
+        <EndorseModal open={open} setOpen={setOpen}/>
       </>
     );
   }
